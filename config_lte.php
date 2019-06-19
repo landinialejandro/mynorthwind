cb92a700fa4aff02103e60e091ac1b7cb700152c
landinialejandro <landinialejandro@gmail.com>
2019-06-19 18:37:02 -0300

    commit

--- config_lte.php
+++ config_lte.php
@@ -2,17 +2,17 @@
 //change to FALSE if you want back to appgini default
 $LTE_globals =[
     "app-title-prefix" => "Ale | ", //window bar prfix title or browser tab
-    "logo-mini" => "glyphicon glyphicon-home", //mini logo for sidebar mini 50x50 pixels
-    "logo-mini-text" => "ALE", // text for side bar
-    "navbar-text" => "Alejandro Landini template",
+    "logo-mini" => "glyphicon glyphicon-tags", //mini logo for sidebar mini 50x50 pixels
+    "logo-mini-text" => "LTE", // text for side bar
+    "navbar-text" => "Alejandro Landini template AdminLTE",
     "footer-left-text" => "<strong>ALE © ". date("Y") ." <a href=\"#\">Alejandro Landini admin template from LTE Admin</a>.</strong>",
     "footer-right-text" => "Anything you want"
 ];
 
  //changue this for tablename icon
  $ico_menu = '{
-    "Orders":"fa fa-table",
-    "Gift":"fa fa-gift",
+    "logins":"fa fa-table",
+    "Locations":"fa fa-gift",
     "Pencil":"fa fa-pencil-square-o",
     "Cog":"fa fa-cog",
     "Plus":"fa fa-plus",
