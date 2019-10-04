114461c0d283420a2fbe87ab45f54ccd2c855a24
landinialejandro <landinialejandro@gmail.com>
2019-10-04 10:35:45 -0300

    toggle off plugins folder

--- plugins/plugins-resources/upload-ajax.php
+++
@@ -1,5 +0,0 @@
-<?php
-	include(dirname(__FILE__) . '/AppGiniPlugin.php');
-	
-	$plugin = new AppGiniPlugin();
-	$plugin->process_ajax_upload();
--- plugins/summary_reports/footer.php
+++
@@ -1,4 +0,0 @@
-			<?php echo $summary_reports->git_branch(); ?>
-		</div> <!-- /div.container -->
-	</body>
-</html>
