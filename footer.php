01ebf465690b8d3577b15ea8c727adbfc944ae93
Alejandro Landini - nbkani <landinialejandro@gmail.com>
2019-12-07 19:34:11 -0300

    delete files, prepare to add library

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-		<?php /* Inserted by Landini AdminLTE Template on 2019-11-18 02:47:39 */ ?>
+		<?php /* Inserted by Landini AdminLTE Template on 2019-11-24 08:54:45 */ ?>
 		
 	<?php
 	//enable LTE Admin
@@ -13,6 +13,88 @@
 	?>
 	
 		<?php /* End of Landini AdminLTE Template code */ ?>
+
+<!-- Add footer template above here -->
+			<div class="clearfix"></div>
+			<?php if(!$_REQUEST['Embedded']) { ?>
+				<div style="height: 70px;" class="hidden-print"></div>
+			<?php } ?>
+
+			<?php if(!$_REQUEST['Embedded']) { ?>
+				<!-- AppGini powered by notice -->
+				<div style="height: 60px;" class="hidden-print"></div>
+				<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+					<p class="navbar-text"><small>
+						Powered by <a class="navbar-link" href="https://bigprof.com/appgini/" target="_blank">BigProf AppGini 5.80</a>
+					</small></p>
+				</nav>
+			<?php } ?>
+
+		</div> <!-- /div class="container" -->
+		<?php if(!defined('APPGINI_SETUP') && is_file(dirname(__FILE__) . '/hooks/footer-extras.php')) { include(dirname(__FILE__).'/hooks/footer-extras.php'); } ?>
+		<script src="<?php echo PREPEND_PATH; ?>resources/lightbox/js/lightbox.min.js"></script>
+	</body>
+</html><!-- Add footer template above here -->
+			<div class="clearfix"></div>
+			<?php if(!$_REQUEST['Embedded']) { ?>
+				<div style="height: 70px;" class="hidden-print"></div>
+			<?php } ?>
+
+			<?php if(!$_REQUEST['Embedded']) { ?>
+				<!-- AppGini powered by notice -->
+				<div style="height: 60px;" class="hidden-print"></div>
+				<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+					<p class="navbar-text"><small>
+						Powered by <a class="navbar-link" href="https://bigprof.com/appgini/" target="_blank">BigProf AppGini 5.80</a>
+					</small></p>
+				</nav>
+			<?php } ?>
+
+		</div> <!-- /div class="container" -->
+		<?php if(!defined('APPGINI_SETUP') && is_file(dirname(__FILE__) . '/hooks/footer-extras.php')) { include(dirname(__FILE__).'/hooks/footer-extras.php'); } ?>
+		<script src="<?php echo PREPEND_PATH; ?>resources/lightbox/js/lightbox.min.js"></script>
+	</body>
+</html><!-- Add footer template above here -->
+			<div class="clearfix"></div>
+			<?php if(!$_REQUEST['Embedded']) { ?>
+				<div style="height: 70px;" class="hidden-print"></div>
+			<?php } ?>
+
+			<?php if(!$_REQUEST['Embedded']) { ?>
+				<!-- AppGini powered by notice -->
+				<div style="height: 60px;" class="hidden-print"></div>
+				<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+					<p class="navbar-text"><small>
+						Powered by <a class="navbar-link" href="https://bigprof.com/appgini/" target="_blank">BigProf AppGini 5.80</a>
+					</small></p>
+				</nav>
+			<?php } ?>
+
+		</div> <!-- /div class="container" -->
+		<?php if(!defined('APPGINI_SETUP') && is_file(dirname(__FILE__) . '/hooks/footer-extras.php')) { include(dirname(__FILE__).'/hooks/footer-extras.php'); } ?>
+		<script src="<?php echo PREPEND_PATH; ?>resources/lightbox/js/lightbox.min.js"></script>
+	</body>
+</html><!-- Add footer template above here -->
+			<div class="clearfix"></div>
+			<?php if(!$_REQUEST['Embedded']) { ?>
+				<div style="height: 70px;" class="hidden-print"></div>
+			<?php } ?>
+
+			<?php if(!$_REQUEST['Embedded']) { ?>
+				<!-- AppGini powered by notice -->
+				<div style="height: 60px;" class="hidden-print"></div>
+				<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+					<p class="navbar-text"><small>
+						Powered by <a class="navbar-link" href="https://bigprof.com/appgini/" target="_blank">BigProf AppGini 5.80</a>
+					</small></p>
+				</nav>
+			<?php } ?>
+
+		</div> <!-- /div class="container" -->
+		<?php if(!defined('APPGINI_SETUP') && is_file(dirname(__FILE__) . '/hooks/footer-extras.php')) { include(dirname(__FILE__).'/hooks/footer-extras.php'); } ?>
+		<script src="<?php echo PREPEND_PATH; ?>resources/lightbox/js/lightbox.min.js"></script>
+	</body>
+</html>
 <!-- Add footer template above here -->
 			<div class="clearfix"></div>
 			<?php if(!$_REQUEST['Embedded']) { ?>
--- plugins/lte/app-resources/jsonsave.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $fichero = 'config.json';
-    $actual = $_POST['json'];
-    // Escribe el contenido al archivo
-    file_put_contents($fichero, $actual);
-
-    echo  $actual;
-    return;
