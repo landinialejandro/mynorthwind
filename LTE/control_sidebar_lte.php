f8ef82d1d40d596aa3a1143152a3d31bc8167754
Alejandro Landini - nbkani <landinialejandro@gmail.com>
2020-01-05 19:07:07 -0300

    fix tabs in right control menu

--- LTE/control_sidebar_lte.php
+++ LTE/control_sidebar_lte.php
@@ -1,29 +1,43 @@
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
     <!-- Create the tabs -->
-    <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-      <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-      <li><a href="#control-sidebar-stats-tab" data-toggle="tab"><i class="fa fa-plus"></i></a></li>
-      <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
+    <ul class="nav nav-tabs nav-justified ">
+      <li class="nav-item">
+        <a href="#control-sidebar-home-tab" data-toggle="tab">
+          <i class="fa fa-home"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#control-sidebar-stats-tab" data-toggle="tab">
+          <i class="fa fa-plus"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#control-sidebar-settings-tab1" data-toggle="tab">
+          <i class="fa fa-cog"></i>
+        </a>
+      </li>
     </ul>
     <!-- Tab panes -->
-    <div class="tab-content" style="padding: 10px; border: 0; border-top: none; " >
+    <div class="tab-content" >
       <!-- Home tab content -->
       <div class="tab-pane" id="control-sidebar-home-tab">
-        <h3 class="control-sidebar-heading">Recent Activity</h3>
+        <h4 class="control-sidebar-heading">Recent Activity</h4>
       </div>
       <!-- /Home .tab-pane -->
+
       <!-- Stats tab content -->
       <div class="tab-pane" id="control-sidebar-stats-tab">
-        <h3 class="control-sidebar-heading">Stats Tab Content</h3>
+        <h4 class="control-sidebar-heading">Stats Tab Content</h4>
       </div>
       <!-- /Stats .tab-pane -->
+
       <?php if(getLoggedAdmin()){ ?>
         <!-- Settings tab content -->
-        <div class="tab-pane" id="control-sidebar-settings-tab">
-          <h3 class="control-sidebar-heading">General Settings</h3>
+        <div class="tab-pane" id="control-sidebar-settings-tab1">
+          <h4 class="control-sidebar-heading">General Settings</h4>
           <div class="col-xs-6 text-center">
-              <a href="<?php echo PREPEND_PATH; ?>LTE/jsonedit.php" class="btn btn-app" title="Open app anviroment options"><i class="fa fa-cogs"></i>&nbsp;App anviroment</a>
+              <a href="<?php echo PREPEND_PATH; ?>LTE/config_edit.php" class="btn btn-app" title="Open app anviroment options"><i class="fa fa-cogs"></i>&nbsp;App anviroment</a>
           </div>
         </div>
         <!-- /Settings .tab-pane -->
