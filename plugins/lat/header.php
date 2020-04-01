cc00fa3f47852670571db7009ed21353b48af8c4
landinialejandro <landinialejandro@gmail.com>
2020-04-01 16:17:39 -0300

    fix plugin

--- plugins/lat/header.php
+++ plugins/lat/header.php
@@ -24,13 +24,13 @@
 
 		<title>Landini Admin Template for AppGini</title>
 		
-		<link id="browser_favicon" rel="shortcut icon" href="<?php echo PREPEND_PATH; ?>plugins/LAT/template.min.png">
+		<link id="browser_favicon" rel="shortcut icon" href="<?php echo PREPEND_PATH; ?>plugins/lat/template.min.png">
 
 		<?php echo $lat_class->get_theme_css_links(); ?>
 		
 		<link rel="stylesheet" href="<?php echo PREPEND_PATH; ?>dynamic.css.php">
 		
-		<link rel="stylesheet" href="<?php echo PREPEND_PATH; ?>plugins/LAT/app-resources/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="<?php echo PREPEND_PATH; ?>plugins/lat/app-resources/font-awesome/css/font-awesome.min.css">
 
 		<!-- jquery ui -->
 		<link rel="stylesheet" href="<?php echo PREPEND_PATH; ?>plugins/plugins-resources/jquery-ui/jquery-ui.min.css">
