023cc3c087b119f495fa79b2c6be65fe8d04355a
landinialejandro <landinialejandro@hotmail.com>
2020-08-02 13:31:35 -0300

    fix sidebar

--- LAT/sideBarMenu_lat.php
+++ LAT/sideBarMenu_lat.php
@@ -5,10 +5,10 @@ if (!isset($Translation)) {
 	include("{$currDir}/../language.php");
 }
 ?>
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar elevation-4 sidebar-dark-primary">
     <!-- Brand Logo -->
     <a href="<?php echo PREPEND_PATH; ?>index.php" class="brand-link">
-        <img src="<?php echo PREPEND_PATH; ?>LAT/logo/L2.png" alt="lat" class="brand-image img-rounded elevation-3" style="opacity: .8">
+        <img src="<?php echo PREPEND_PATH; ?>LAT/logo/tradexx_simple.png" alt="lat" class="brand-image img-rounded elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light"><?php echo $LAT_globals['app-brand-text']; ?></span>
     </a>
 
@@ -24,7 +24,7 @@ if (!isset($Translation)) {
         </div>
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
                     <a href="<?php echo PREPEND_PATH; ?>index.php?signOut=1" class="nav-link">
                         <i class="nav-icon fas fa-sign-out-alt text-info"></i>
@@ -34,135 +34,98 @@ if (!isset($Translation)) {
                 <div class="pb-3 mb-3" style="border-bottom: 1px solid #4f5962;"></div>
 
                 <?php
-                /* accessible tables */
 
-                $arrTables = get_tables_info();
                 $homeLinks = [];
+                $len = 17;
                 @include("{$currDir}/hooks/links-home.php");
-                if (is_array($arrTables) && count($arrTables)) {
-                    /* how many table groups do we have? */
-                    $groups = get_table_groups();
-                    $multiple_groups = (count($groups) > 1 ? true : false);
-
-                    /* construct $tg: table list grouped by table group */
-                    $tg = array();
-                    //echo get_list($groups);
-                    if (count($groups)) {
-                        foreach ($groups as $grp => $tables) {
-                            foreach ($tables as $tn) {
-                                $tg[$tn] = $grp;
-                                if ($tn === $x->TableName) {
-                                    $current_group = $grp;
-                                }
-                            }
-                        }
-                    }
-                    $i = 0;
-                    $len = 17;
-                    foreach ($groups as $lat_group => $lat_tables) {
-                        if (($lat_group !== 'hiddens' || $memberInfo['admin'])) { // new fucntionality if table group named hiddens dont show in other users
-                            if (count($lat_tables)) {
-                                if (($lat_group !== 'None')) {
-                ?>
-                                    <li class="nav-item has-treeview <?php echo ($lat_group === $current_group ? 'menu-open' : ''); ?>">
-                                        <a href="#" class="nav-link">
-                                            <i class="nav-icon <?php echo $LAT_group_ico[$lat_group] ? $LAT_group_ico[$lat_group] : $ico; ?>"></i>
-                                            <p>
-                                                <?php echo $lat_group; ?>
-                                                <i class="right fas fa-angle-left"></i>
-                                            </p>
-                                        </a>
-                                        <ul class="nav nav-treeview">
-                                            <?php
-                                        }
-                                        foreach ($lat_tables as $lat_table) {
-                                            $tc = $arrTables[$lat_table];
-                                            $count_badge = '';
-                                            if ($tc['homepageShowCount']) {
-                                                $sql_from = get_sql_from($lat_table);
-                                                $count_records = ($sql_from ? sqlValue("select count(1) from " . $sql_from) : 0);
-                                                $count_badge = '<spam class="right badge badge-info">' . number_format($count_records) . '</spam>';
-                                            }
-                                            /* hide current table in homepage? */
-                                            $tChkHL = array_search($lat_table, array('ordersDetails', 'creditDocument', '_resumeOrders', 'electronicInvoice', 'modalitaPagamento', 'codiceDestinatario', 'regimeFiscale', 'tipoCassa'));
-                                            if ($tChkHL === false || $tChkHL === null) { /* if table is not set as hidden in homepage */ ?>
-                                                <li class="nav-item">
-                                                    <a href="<?php echo PREPEND_PATH . $lat_table; ?>_view.php" class="nav-link  <?php echo ($lat_table === $x->TableName ? 'active' : ''); ?>">
-                                                        <?php echo ($tc['tableIcon'] ? '<img src="' . PREPEND_PATH . $tc['tableIcon'] . '">' : ''); ?>
-                                                        <p>
-                                                            <?php
-                                                            $dot = (strlen($tc['Caption']) > $len) ? "..." : "";
-                                                            echo substr($tc['Caption'], 0, $len) . $dot;
-                                                            echo $count_badge;
-                                                            ?>
-                                                        </p>
-                                                    </a>
-                                                </li>
-                                            <?php
-                                            }
-                                        }
-                                        foreach ($homeLinks as $link) {
-                                            if (!isset($link['url']) || !isset($link['title'])) continue;
-                                            if ($lat_group != $link['table_group'] && $lat_group != '*') continue;
-                                            if ($memberInfo['admin'] || @in_array($memberInfo['group'], $link['groups']) || @in_array('*', $link['groups'])) {
-                                                $title = $link['subGroup'] ? $link['subGroup'] . " - " . $link['title'] : $link['title'];
-                                                $dot = (strlen($title) > $len + 3) ? "..." : "";
-                                            ?>
-                                                <li class="nav-item">
-                                                    <a href="<?php echo PREPEND_PATH . $link['url']; ?>" title="<?php echo $title; ?>" class="nav-link">
-                                                        <?php echo ($link['icon'] ? '<img src="' . PREPEND_PATH . $link['icon'] . '">' : ''); ?>
-                                                        <p>
-                                                            <?php echo substr($title, 0, $len + 3) . $dot; ?>
-                                                        </p>
-                                                    </a>
-                                                </li>
-                                            <?php
-                                            }
-                                        }
-                                        if (($lat_group !== 'None')) {
-                                            ?>
-
-                                        </ul>
-                                    </li>
-                                <?php
-                                        }
-                                        $i++;
-                                    } else {
-                                ?>
-                                <li class="nav-item active">
-                                    <a href="#" class="nav-link">
-                                        <i class="fa fa-link"></i>
-                                        <span><?php echo $lat_group; ?></span>
+                $arrTables = get_tables_info();
+                $groups = get_table_groups();
+                foreach ($groups as $lte_group => $lte_tables) {
+                    if (($lte_group !== 'hiddens' || $memberInfo['admin'])) { // new fucntionality if table group named hiddens dont show in other users
+                        if (count($lte_tables)) {
+                            if (($lte_group !== 'None')) { ?>
+                                <li class="nav-item has-treeview <?php echo (in_array($x->TableName, $lte_tables)  ? 'menu-open' : ''); ?>">
+                                    <a href="#" class="nav-link <?php echo (in_array($x->TableName, $lte_tables)  ? 'active' : ''); ?>">
+                                        <i class="nav-icon <?php echo $LTE_group_ico[$lte_group] ? $LTE_group_ico[$lte_group] : $ico; ?>"></i>
+                                        <p>
+                                            <?php echo $lte_group; ?>
+                                            <i class="right fas fa-angle-left"></i>
+                                        </p>
                                     </a>
-                                </li>
+                                    <ul class="nav nav-treeview">
+                                    <?php
+                                    }
+                                    foreach ($lte_tables as $lte_table) {
 
+                                        $tc = $arrTables[$lte_table];
+                                        ?>
+                                            <li class="nav-item">
+                                                <a href="<?php echo PREPEND_PATH . $lte_table; ?>_view.php" class="nav-link  <?php echo ($lte_table === $x->TableName ? 'active' : ''); ?>">
+                                                    <?php echo ($tc['tableIcon'] ? '<img src="' . PREPEND_PATH . $tc['tableIcon'] . '">' : ''); ?>
+                                                    <p>
+                                                        <?php
+                                                        $dot = (strlen($tc['Caption']) > $len) ? "..." : "";
+                                                        echo substr($tc['Caption'], 0, $len) . $dot;
+                                                        echo counter($lta_table, $tc);
+                                                        ?>
+                                                    </p>
+                                                </a>
+                                            </li>
+                                            <?php
+                                    }
+                                    foreach ($homeLinks as $link) {
+                                        if (!isset($link['url']) || !isset($link['title'])) continue;
+                                        if ($lte_group != $link['table_group'] && $lte_group != '*') continue;
+                                        if ($memberInfo['admin'] || @in_array($memberInfo['group'], $link['groups']) || @in_array('*', $link['groups'])) {
+                                            $title = $link['subGroup'] ? $link['subGroup'] . " - " . $link['title'] : $link['title'];
+                                            $dot = (strlen($title) > $len + 3) ? "..." : "";
+                                        ?>
+                                            <li class="nav-item">
+                                                <a href="<?php echo PREPEND_PATH . $link['url']; ?>" title="<?php echo $title; ?>" class="nav-link">
+                                                    <?php echo ($link['icon'] ? '<img src="' . PREPEND_PATH . $link['icon'] . '">' : ''); ?>
+                                                    <p>
+                                                        <?php echo substr($title, 0, $len + 3) . $dot; ?>
+                                                    </p>
+                                                </a>
+                                            </li>
+                                        <?php
+                                        }
+                                    }
+                                    if (($lte_group !== 'None')) {
+                                        ?>
+                                    </ul>
+                                </li>
                             <?php
                                     }
-                                }
-                            }
-                            foreach ($homeLinks as $link) {
-                                if (!isset($link['url']) || !isset($link['title'])) continue;
-                                if ($link['table_group'] != '*' && $link['table_group'] != '') continue;
-                                if ($memberInfo['admin'] || @in_array($memberInfo['group'], $link['groups']) || @in_array('*', $link['groups'])) {
+                                } else {
                             ?>
-                            <li class="nav-item">
-                                <a href="<?php echo PREPEND_PATH . $link['url']; ?>" class="nav-link">
-                                    <?php echo ($link['icon'] ? '<img src="' . PREPEND_PATH . $link['icon'] . '">' : ''); ?>
-                                    <p>
-                                        <?php echo $link['subGroup'] ? $link['subGroup'] . " - " . $link['title'] : $link['title']; ?>
-                                    </p>
+                            <li class="nav-item active">
+                                <a href="#" class="nav-link">
+                                    <i class="fa fa-link"></i>
+                                    <span><?php echo $lte_group; ?></span>
                                 </a>
                             </li>
-                    <?php
+                        <?php
                                 }
                             }
-                        } else {
-                    ?><script>
-                        window.location = 'index.php?signIn=1';
-                    </script><?php
-                            }
-                                ?>
-
+                }
+                foreach ($homeLinks as $link) {
+                    if (!isset($link['url']) || !isset($link['title'])) continue;
+                    if ($link['table_group'] != '*' && $link['table_group'] != '') continue;
+                    if ($memberInfo['admin'] || @in_array($memberInfo['group'], $link['groups']) || @in_array('*', $link['groups'])) {
+                        ?>
+                        <li class="nav-item">
+                            <a href="<?php echo PREPEND_PATH . $link['url']; ?>" class="nav-link">
+                                <?php echo ($link['icon'] ? '<img src="' . PREPEND_PATH . $link['icon'] . '">' : ''); ?>
+                                <p>
+                                    <?php echo $link['subGroup'] ? $link['subGroup'] . " - " . $link['title'] : $link['title']; ?>
+                                </p>
+                            </a>
+                        </li>
+                        <?php
+                    }
+                }
+                ?>
             </ul>
             <!-- /.sidebar-menu -->
         </nav>
@@ -171,45 +134,13 @@ if (!isset($Translation)) {
 </aside>
 
 <?php
-//TODO: finish this function to make side bar menu
-function get_list($list, $f = true)
+function counter($table, $tc)
 {
-    if (!is_array($list)) return '';
-    $items = array();
-    foreach ($list as $ls => $names) {
-        if (is_array($names)) {
-            $subsIt = get_list(array_flip($names), false);
-            $has_treeview = "has-treeview";
-            $right = ' <i class="right fas fa-angle-left"></i>';
-            $icon = '<i class="nav-icon fa fa-table"></i>';
-            $caption = $ls;
-            $href = "#";
-        } else {
-            $tables = get_tables_info();
-            $table=$tables[$ls];
-            $caption = $table['Caption'];
-            $icon="<img src= '". PREPEND_PATH . $table['tableIcon'] ."' >";
-            $href = PREPEND_PATH . "{$ls}_view.php";
-            if ($table['homepageShowCount']) {
-                $sql_from = get_sql_from($ls);
-                $count_records = ($sql_from ? sqlValue("select count(1) from " . $sql_from) : 0);
-                $count_badge = '<spam class="right badge badge-info">' . number_format($count_records) . '</spam>';
-            }
-        }
-        $items[] =  "<li class='nav-item {$has_treeview}'>".
-                        "<a href='{$href}' class='nav-link'>". $icon.
-                            "<p>{$caption}{$count_badge}{$right}</p>".
-                        "</a>{$subsIt}".
-                    "</li>";
+    $count_badge = '';
+    if ($tc['homepageShowCount']) {
+        $sql_from = get_sql_from($table);
+        $count_records = ($sql_from ? sqlValue("select count(1) from " . $sql_from) : 0);
+        $count_badge = '<spam class="right badge badge-info">' . number_format($count_records) . '</spam>';
     }
-    if (count($items)) {
-        if ($f) {
-            $ret = '<li class="">' . implode('', $items) . '</li>';
-        } else {
-            $ret = '<ul class="nav nav-treeview">' . implode('', $items) . '</ul>';
-        };
-        return $ret;
-    } else {
-        return '';
-    }
+    return $count_badge;
 }
